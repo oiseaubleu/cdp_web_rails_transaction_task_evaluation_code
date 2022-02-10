47e06f04a115d6135d700b4e0e74bbd9c3c9587d
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2022-02-10 21:00:13 +0900

    Update order_spec.rb

--- models/order_spec.rb
+++ models/order_spec.rb
@@ -1,35 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
-  describe "発注機能" do
-    let!(:user1){FactoryBot.create(:user)}
-    let!(:user2){FactoryBot.create(:second_user)}
-    let!(:item){FactoryBot.create(:item)}
+RSpec.describe "発注機能", type: :model do
+  let!(:user1){FactoryBot.create(:user)}
+  let!(:user2){FactoryBot.create(:second_user)}
+  let!(:item){FactoryBot.create(:item)}
 
-
-    context "2人のユーザが同じ商品を同時に5個ずつ注文した場合" do
-      before do
-        threads = []
-        threads << Thread.new do
-          ActiveRecord::Base.connection_pool.with_connection do
-            order = Order.create(user_id: user1.id)
-            ordered_list = OrderedList.create(order_id: order.id, item_id: item.id, quantity: 5)
-            order.update_total_quantity
-          end
+  context "2人のユーザが同じ商品を同時に5個ずつ注文した場合" do
+    before do
+      threads = []
+      threads << Thread.new do
+        ActiveRecord::Base.connection_pool.with_connection do
+          order = Order.create(user_id: user1.id)
+          ordered_list = OrderedList.create(order_id: order.id, item_id: item.id, quantity: 5)
+          order.update_total_quantity
         end
-        threads << Thread.new do
-          ActiveRecord::Base.connection_pool.with_connection do
-            order = Order.create(user_id: user2.id)
-            ordered_list = OrderedList.create(order_id: order.id, item_id: item.id, quantity: 5)
-            order.update_total_quantity
-          end
+      end
+      threads << Thread.new do
+        ActiveRecord::Base.connection_pool.with_connection do
+          order = Order.create(user_id: user2.id)
+          ordered_list = OrderedList.create(order_id: order.id, item_id: item.id, quantity: 5)
+          order.update_total_quantity
         end
-        threads.each(&:join)
       end
-
-      it "その商品の注文数が10になること" do
-        expect(item.reload.total_quantity).to eq(5 + 5)
-      end
+      threads.each(&:join)
+    end
+    it "その商品の注文数が10個になること" do
+      expect(item.reload.total_quantity).to eq(5 + 5)
     end
   end
 end
